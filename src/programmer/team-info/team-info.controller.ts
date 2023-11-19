6b22d4623ffbd8eed94d803d44019b1c8d2612a1
nh-shohan <nh.shohan6@gmail.com>
2023-11-20 02:49:21 +0600

    team information service complete

--- src/programmer/team-info/team-info.controller.ts
+++ src/programmer/team-info/team-info.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Patch,
@@ -12,55 +14,118 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { TeamInfoDTO } from './team-info.dto';
+import { TeamInfoService } from './team-info.service';
 import { UpdateTeamNameDTO } from './update-team-name.dto';
 
 @Controller('programmer')
 export class TeamInfoController {
-  //   Team Post Request
+  constructor(private teamInfoService: TeamInfoService) {}
+
+  // Team Post Request
   @Post('team')
   @UsePipes(new ValidationPipe())
   createTeam(@Body() teamInfo: TeamInfoDTO) {
-    return teamInfo;
+    try {
+      return this.teamInfoService.postTeamInformation(teamInfo);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  //   Get All Teams Information
+  // Get All Teams Information
   @Get('team')
   @UsePipes(new ValidationPipe())
-  getAllTeams(@Body() teamInfo: TeamInfoDTO) {
-    return teamInfo;
+  getAllTeams() {
+    try {
+      return this.teamInfoService.getAllTeamInfo();
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  //   Get single Team Information
+  // Get single Team Information
   @Get('team/:teamId')
   @UsePipes(new ValidationPipe())
   getSingleTeam(@Param('teamId', ParseIntPipe) teamId: number) {
-    return teamId;
+    try {
+      return this.teamInfoService.getTeamInfoById(teamId);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  //   Update Team Information
+  // Update Team Information
   @Put('team/:teamId')
   @UsePipes(new ValidationPipe())
   updateTeam(
     @Param('teamId', ParseIntPipe) teamId: number,
     @Body() teamInfo: TeamInfoDTO,
   ) {
-    return { teamId, teamInfo };
+    try {
+      return this.teamInfoService.updateTeamInfo(teamId, teamInfo);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  //   Update Team Name
+  // Update Team Name
   @Patch('team/:teamId')
   @UsePipes(new ValidationPipe())
   updateTeamName(
     @Param('teamId', ParseIntPipe) teamId: number,
     @Body() teamInfo: UpdateTeamNameDTO,
   ) {
-    return { teamId, teamInfo };
+    try {
+      return this.teamInfoService.updateTeamName(teamId, teamInfo.newTeamName);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  //   Delete Team
+  // Delete Team
   @Delete('team/:teamId')
   @UsePipes(new ValidationPipe())
   deleteTeam(@Param('teamId', ParseIntPipe) teamId: number) {
-    return teamId;
+    try {
+      return this.teamInfoService.deleteTeamInfo(teamId);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
