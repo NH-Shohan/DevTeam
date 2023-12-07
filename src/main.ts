84477fa3de14aa2c8673d0291cdfc7acb71fca67
nh-shohan <nh.shohan6@gmail.com>
2023-12-08 01:49:24 +0600

    merged

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,9 @@
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-  app.use(
-    session({
-      secret: 'DevTeam_Secret',
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 86400000, // 1 day
-=======
   // somewhere in your initialization file
   app.use(
     session({
@@ -23,10 +13,11 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 300000,
->>>>>>> a5a257349232ca21b9136ba7df2b3989b9d04c0a
       },
     }),
   );
   await app.listen(3000);
 }
 bootstrap();
+
+// Programmer comment
