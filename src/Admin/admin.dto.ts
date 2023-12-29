1d064ca70ac9ef7794829294e1eb808e441f05df
Md. Jidanul Hakim Jitu <20-44365-3@student.aiub.edu>
2023-12-29 18:20:09 +0600

    frontend added

--- src/Admin/admin.dto.ts
+++ src/Admin/admin.dto.ts
@@ -4,25 +4,46 @@ import { IsEmail, IsString, Length, Matches, MaxLength } from 'class-validator';
 export class ValidateAdminProfile {
   id: number;
   @IsString()
-  @Matches(/^[A-Za-z]+$/, {
-    message: 'Name must be string',
+  @Length(4, 30, {
+    message: 'Name must be between 4 and 30 characters',
   })
-  @MaxLength(30)
   name: string;
 
-  @IsEmail()
+  @IsString()
+  @Matches(/^[a-z0-9]+$/, {
+    message: 'Username must contain only lowercase letters and numbers',
+  })
+  username: string;
+
+  @IsEmail({}, { message: 'Invalid email address' })
   email: string;
 
-  @Length(2, 30, {
-    message: 'Leanth can not be less than 2 and more than 30 character.',
+  @Length(8, undefined, {
+    message: 'Password must be at least 8 characters',
   })
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-    message:
-      'Password must be minimum 8 characters, at least one letter, one number and one special character',
+  @Matches(/^(?=.*[A-Z])/i, {
+    message: 'Password must contain at least 1 uppercase letter',
   })
   password: string;
 
+  @Length(10, 10, {
+    message: 'Invalid National ID',
+  })
+  nationalId: string;
+
+  // You may need additional validation rules for the photo property
+
+  // @MaxLength(30, {
+  //   message: 'Image name must be at most 30 characters',
+  // })
   imageName: string;
+
+  // Ensure to match the DTO with the form structure
+  photo: string; // Assuming it's a FileList, you may need to adjust this based on your form implementation
+
+  role: 'executive' | 'moderator';
+
+  permissions: ('creating' | 'adding' | 'deleting')[];
 }
 
 export class ValidateModeratorProfile {
--- src/Admin/admin.entity.ts
+++ src/Admin/admin.entity.ts
@@ -12,27 +12,46 @@ import {
 export class AdminEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   name: string;
+
+  @Column()
+  username: string;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
   @Column()
-  imageName: string;
+  nationalId: string;
+
+  @Column({ nullable: true }) // Allow null values for imageName
+  imageName: string | null;
+
+  // Additional fields based on ValidateAdminProfile DTO
+  @Column()
+  photo: string; // Assuming it's a FileList, you may need to adjust this based on your form implementation
+
+  @Column({
+    type: 'enum',
+    enum: ['executive', 'moderator'],
+  })
+  role: 'executive' | 'moderator';
+
+  @Column('simple-array', { default: [] })
+  permissions: ('creating' | 'adding' | 'deleting')[];
 }
 
 @Entity('All3Relation')
 export class RelationEntity {
   @PrimaryGeneratedColumn()
   id: number;
-  @OneToOne(
-    () => ProfileEntity,
-    (candidateProfile) => candidateProfile.email,
-    {
-      cascade: true,
-    },
-  )
+  @OneToOne(() => ProfileEntity, (candidateProfile) => candidateProfile.email, {
+    cascade: true,
+  })
   @JoinColumn()
   candidateProfile: ProfileEntity;
 
