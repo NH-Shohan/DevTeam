35a0504f246b2a1a96777259b2693dcded50e131
Md. Jidanul Hakim Jitu <20-44365-3@student.aiub.edu>
2023-12-30 23:32:45 +0600

    specific user fixed

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,33 @@
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // somewhere in your initialization file
+
+  // const corsOptions: CorsOptions = {
+  //   origin: 'http://localhost:3000', // Specify your frontend origin
+  //   credentials: true, // Allow credentials (cookies)
+  // };
+
   app.use(
     session({
       secret: 'my-secret',
       resave: false,
       saveUninitialized: false,
       cookie: {
-        maxAge: 300000,
+        maxAge: 30000000,
       },
     }),
   );
 
   // Enable CORS
   // Enable CORS
-  app.enableCors();
+
+  // app.enableCors(corsOptions);
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
 
   await app.listen(3333);
 }
